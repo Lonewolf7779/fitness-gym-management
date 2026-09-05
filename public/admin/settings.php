1ed2d8f8bc15d16bfcf4f08414ac307b7c1c9a04
Prajapati Santosh <sntoshprajapati163@gmail.com>
2026-09-05 12:06:09 +0530

    feat: route settings to functional view

--- public/admin/settings.php
+++ public/admin/settings.php
@@ -1,3 +1,3 @@
 <?php
 require_once __DIR__ . '/../../app/config/config.php';
-require_once __DIR__ . '/../../app/views/admin/settings.php';
+require_once __DIR__ . '/../../app/views/admin/settings-live.php';
